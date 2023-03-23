c73f49b85577662f7f5726c63c2981bcf1003d32
Alarid52 <alarid152@gmail.com>
2023-03-23 17:24:18 -0600

    Codigo sobrante

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,4 @@
 import Head from 'next/head'
-import Image from 'next/image'
-import styles from '@/styles/Home.module.css'
 import { NavBar } from '@/components/NavBar'
 import { GridCategories } from '@/components/GridCategories'
 import { useEffect, useState } from 'react'
