dfcb7d5180434e53794b36386ca90259060089b3
Alarid52 <alarid152@gmail.com>
2023-03-27 17:04:03 -0600

    FIX: Precio total refactorizado

--- src/components/CartMenu.jsx
+++ src/components/CartMenu.jsx
@@ -2,14 +2,12 @@ import { MenuCard } from './MenuCard';
 
 export const CartMenu = ({ itemsCartMenu, buttons }) => {
 
-    const totalPrice = (itemsCartMenu.length === 0) ? 0 : itemsCartMenu.map((item) => item.quantity * item.price).reduce((prev, next) => prev + next);
-
     return (
         <div className="menu-cart">
             {itemsCartMenu.map((item) => <MenuCard key={item.id} item={item} buttons={buttons} />)}
             <div className="total-price">
                 <button type="button" className="btn-drop" onClick={buttons.delAll}>Empty Cart</button>
-                <h4>Total Price: ${totalPrice.toFixed(2)}</h4>
+                <h4>Total Price: ${(itemsCartMenu.length === 0) ? 0 : itemsCartMenu.map((item) => item.quantity * item.price).reduce((prev, next) => prev + next).toFixed(2)}</h4>
             </div>
         </div>
     )
